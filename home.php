85ed8505b4bce7cbf5e13897aee7001fcfebeedb
Ellen Liu <huailun@hotmail.com>
2016-04-14 09:36:37 -0600

    DataDesign

--- home.php
+++ home.php
@@ -6,9 +6,9 @@
 	</head>
 	<body>
 		<a href="http://distil.flexmls.com/cgi-bin/mainmenu.cgi">Original example</a>
-		<header><img src="images/gaar.jpg" alt="house" height="5" width="8"></header>
+		<header><img src="images/gaar.jpg" alt="house" height="5" width="8"/></header>
 		<ol>
-			<li>The persona is potential house buyers</li>
+			<li>The persona is potential house buyers & investors</li>
 
 		</ol>
 
