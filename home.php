c182c91dc7769de80b37215562a9abe754699903
Ellen Liu <huailun@hotmail.com>
2016-04-14 09:27:46 -0600

    DataDesign

--- home.php
+++ home.php
@@ -5,7 +5,8 @@
 		<title>DataDesignPhase1</title>
 	</head>
 	<body>
-		<a href="http://distil.flexmls.com/cgi-bin/mainmenu.cgi"></a>
+		<a href="http://distil.flexmls.com/cgi-bin/mainmenu.cgi">Original example</a>
+		<img src="images/gaar.jpg"/>
 		<ol>
 			<li>The persona is potential house buyers</li>
 
